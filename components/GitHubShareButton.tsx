db156dcd90f574e1ff25f83f593c17f3879c25f4
Ytnxne571 <123819213+Ytnxne571@users.noreply.github.com>
2023-04-15 07:26:07 +0800

    Update GitHubShareButton.tsx

--- components/GitHubShareButton.tsx
+++ components/GitHubShareButton.tsx
@@ -1,19 +1,35 @@
-export const GitHubShareButton: React.FC = () => {
-  const [visible, setVisible] = useState(true);
+import { useState } from 'react';
+import styles from './GitHubShareButton.module.css';
 
-  return visible ? (
+export const GitHubShareButton: React.FC = () => {
+  // 初始值为 false，表示图标不可见
+  const [visible, setVisible] = useState(false);
+
+  return (
     <a
       href='https://github.com/transitive-bullshit/nextjs-notion-starter-kit'
       target='_blank'
       rel='noopener noreferrer'
       className={styles.githubCorner}
       aria-label='View source on GitHub'
+      style={{
+        visibility: visible ? 'visible' : 'hidden',
+        opacity: visible ? 1 : 0,
+        transition: 'opacity 0.2s linear'
+      }}
+      onMouseEnter={() => setVisible(true)}
+      onMouseLeave={() => setVisible(false)}
     >
-      <svg width='80' height='80' viewBox='0 0 250 250' style={{ fill: '#70B7FD', color: '#fff', position: 'absolute' }}>
-      {/* 这里是图标的矢量图形代码 */}
+      <svg
+        width='80'
+        height='80'
+        viewBox='0 0 250 250'
+        style={{ fill: '#70B7FD', color: '#fff', position: 'absolute' }}
+      >
+        {/* 这里是 Github 圆角图标的矢量图形代码 */}
       </svg>
-    </a> 
-  ) : null;
+    </a>
+  );
 };
 
       <svg
